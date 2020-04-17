f5502b0d8817a8b6210abd2835255d55c0fbf910
Zack Ankner <zackankner@gmail.com>
2020-04-17 00:21:37 -0400

    feat(master) added script to get users

---
+++ api/users/get.js
@@ -0,0 +1,25 @@
+// Imports:
+const admin = require('firebase-admin');
+const status = require('http-status');
+
+module.exports = async (req, res) => {
+  try {
+    const { uid } = await admin.auth().verifyIdToken(req.headers.authorization);
+    if (uid !== req.params.user) return res.sendStatus(status.UNAUTHORIZED);
+
+    const userRef = admin.firestore().collection('users').doc(uid);
+    const userDoc = await userRef.get();
+    if (!userDoc.data()) return res.sendStatus(status.UNAUTHORIZED);
+
+    return res.send(userDoc.data());
+  } catch(err) {
+    console.log(err.message);
+
+    switch (err.code) {
+      case 'auth/argument-error':
+        return res.sendStatus(status.UNAUTHORIZED);
+      default:
+        return res.sendStatus(status.INTERNAL_SERVER_ERROR);
+    }
+  }
+};

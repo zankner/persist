ffdac776e32c8dea8c290ea62732faf02ccecb4a
Zack Ankner <zackankner@gmail.com>
2020-04-20 20:37:19 -0400

    feat(master) creating billing for businesses

--- api/business/billing/create.js
+++ api/business/billing/create.js
@@ -5,38 +5,36 @@ const check = require('check-types');
 const stripe = require('stripe')(process.env.TEST_STRIPE_SECRET);
 
 module.exports = async (req, res) => {
+  try {
+    const state = JSON.parse(req.query.state);
+    const { auth, business } = state;
 
-  console.log('called');
-  console.log(req);
+    const { uid } = await admin.auth().verifyIdToken(auth);
+    if (!uid) return res.sendStatus(status.UNAUTHORIZED);
 
-  const stripeAccount = await stripe.oauth.token({
-    grant_type: 'authorization_code',
-    code: req.query.code
-  });
+    const businessRef = admin.firestore().collection('businesses').doc(business);
+    const businessDoc = await businessRef.get();
+    if (!businessDoc.data()) return res.sendStatus(status.UNAUTHORIZED);
+    if (!businessDoc.data().admins.includes(uid)) return res.sendStatus(status.UNAUTHORIZED);
 
-  console.log(stripeAccount.stripe_user_id);
+    const stripeAccount = await stripe.oauth.token({
+      grant_type: 'authorization_code',
+      code: req.query.code
+    });
 
-  // try {
-  //   const { uid } = await admin.auth().verifyIdToken(req.query.state);
-  //   if (!uid) return res.sendStatus(status.UNAUTHORIZED);
-  //
-  //   const businessRef = admin.firestore().collection('businesses').doc(req.query.state.business);
-  //   const businessDoc = await businessRef.get();
-  //   if (!businessDoc.data()) return res.sendStatus(status.UNAUTHORIZED);
-  //   if (!businessDoc.data().admins.includes(uid)) return res.sendStatus(status.UNAUTHORIZED);
-  //
-  //
-  //   await businessRef.update({'billing.stripeAccount': })
-  //
-  //   return res.send(order);
-  // } catch(err) {
-  //   console.log(err.message);
-  //
-  //   switch (err.code) {
-  //     case 'auth/argument-error':
-  //       return res.sendStatus(status.UNAUTHORIZED);
-  //     default:
-  //       return res.sendStatus(status.INTERNAL_SERVER_ERROR);
-  //   }
-  // }
+    const accountId = stripeAccount.stripe_user_id;
+
+    await businessRef.update({'billing.stripeAccount': accountId});
+
+    return res.sendStatus(status.OK);
+  } catch(err) {
+    console.log(err.message);
+
+    switch (err.code) {
+      case 'auth/argument-error':
+        return res.sendStatus(status.UNAUTHORIZED);
+      default:
+        return res.sendStatus(status.INTERNAL_SERVER_ERROR);
+    }
+  }
 };

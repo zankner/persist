9c0622258c57ba4f9fcb1d23c298a52764ece1e2
Zack Ankner <zackankner@gmail.com>
2020-06-01 20:54:50 -0400

    feat(master) Working on create business

--- src/pages/Businesses/Create/Create.js
+++ src/pages/Businesses/Create/Create.js
@@ -4,38 +4,35 @@ import { compose } from 'redux';
 import { withFirebase } from 'react-redux-firebase';
 import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Formik, Field, Form, getIn, ErrorMessage } from 'formik';
+import Introduction from "./components/Introduction/Introduction";
+import Background from "./components/Background/Background";
+import Description from "./components/Description/Description";
+import Specifics from "./components/Specifics/Specifics";
+import Photos from "./components/Photos/Photos";
+import Completion from "./components/Completion/Completion";
 
 
 const Create = props => {
 
+  const [slideIndex, setSlideIndex] = useState(0);
+
   return (
     <Layout >
       <div className="pt-nav">
-        <div className="progress rounded-0 sticky-top" style={{height: '8px', top: '78px'}}>
-          <div className="progress-bar" role="progressbar" style={{
-            width: "0%", ariaValuenow: "0", ariaValuemin: "0",
-            ariaValuemax: "100"
-          }}/>
-        </div>
-        <section className="py-5 py-lg-7">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-5">
-                <p className="subtitle text-primary">Add a new business</p>
-                <h1 className="h2 mb-5">Let's get started</h1>
-                <p className="text-muted">Don't let these troubling times stop your business. Instead, adapt your business
-                to meet the challenge of our current situation.</p>
-                <p className="text-muted mb-5">Persist provides commerce that is convenient for both you and your customers.</p>
-                <p className="mb-5 mb-lg-0">
-                  <Link className="btn btn-primary" to="/business/create/background">Add your business</Link>
-                </p>
-              </div>
-              <div className="col-lg-5 ml-auto d-flex align-items-center"><img className="img-fluid"
-                                                                               src="/img/illustration/undraw_celebration_0jvk.svg"
-                                                                               alt="" style={{width: "400px"}} /></div>
-            </div>
-          </div>
-        </section>
+        <Formik
+          initialValues={{
+            name: "Company",
+          }
+          }}
+          onSubmit={handleSubmit}
+        >
+        {slideIndex === 0 && Introduction}
+        {slideIndex === 1 && Background}
+        {slideIndex === 2 && Description}
+        {slideIndex === 3 && Specifics}
+        {slideIndex === 4 && Photos}
+        {slideIndex === 5 && Completion}
       </div>
     </Layout>
   )
